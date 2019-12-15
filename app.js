352c1d5ee0d3bb26fc1cc121038f9570d866b1d7
Linus Falk <linfa084@student.liu.se>
2019-12-15 23:32:45 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.3s ease forwards ${index / 10 +
+          0.3}s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      if (nav.classList.contains("nav-active")) {
+        nav.classList.toggle("nav-active");
+        navLinks.forEach((link, index) => {
+          link.style.animation = "";
+        });
+        burger.classList.toggle("toggle");
+      }
+    });
+  });
+};
+
+navSlide();
